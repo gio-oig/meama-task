5e77eecbcfe699c253647e419165a8a259c15232
gio-oig <araxamiag@gmail.com>
2022-12-09 13:01:18 +0400

    add gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <QueryClientProvider client={queryClient}>
       <ContextProvider>
         <App />
